078d4a5b1955fc45801c91d23fcf6487f0428e9f
DusanZigic <zigic.dusan1991@gmail.com>
2024-03-13 23:48:10 +0100

    removed unnecessary source files

--- source/InputParser.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef HEADERFILE_INPUTHEADER
-#define HEADERFILE_INPUTHEADER
-
-int GetInputs(vector<string> inputs, string &pname, string &cent, double &xb, int &xptsn, int &yptsn, int &phiptsn, string &pTinitpath, string &temppath, string &bcdpath, double &timestep, double &tcrit); //function that gets inputs for energy loss calculations
-int GetInputs(vector<string> inputs, string &pname, double &xb, int &LdndxMaxPts, int &LCollMaxPts);				 //function that gets inputs for LTables calculations
-
-#endif
--- source/MainHeader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef HEADERFILE_MAINHEADER
-#define HEADERFILE_MAINHEADER
-
-void AverageEL(); 		//function that calculates energy loss
-
-void GenerateLTables(); //function that generates LTables
-
-#endif
