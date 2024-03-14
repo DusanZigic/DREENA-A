d9f1f6abe7eac5dd704bff1a04eef4494fe2db9e
DusanZigic <zigic.dusan1991@gmail.com>
2024-03-14 01:38:03 +0100

    added energy loss to main

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
 #include "ltables.hpp"
+#include "energyloss.hpp"
 
 #include <iostream>
 #include <string>
@@ -22,10 +23,10 @@ int main(int argc, char const *argv[])
 		std::cout << "use: 'calculation' -h for extra help" << std::endl;
 		return 0;
 	}
-    // else if (argv[1] ==  std::string("AverageEL")) {
-    //     energyLoss EnergyLoss(argc, argv);
-	// 	EnergyLoss.runEnergyLoss();
-    // }
+    else if (argv[1] ==  std::string("AverageEL")) {
+        energyLoss EnergyLoss(argc, argv);
+		EnergyLoss.runEnergyLoss();
+    }
     else if (argv[1] == std::string("LTables")) {
 		lTables LTables(argc, argv);
 		LTables.runLTables();
